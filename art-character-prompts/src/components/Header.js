e0908def386a0b35836e52d359253f2844baf521
Jaz Kime <JazDKime@gmail.com>
2022-08-30 17:14:08 -0700

    Icon update

--- art-character-prompts/src/components/Header.js
+++ art-character-prompts/src/components/Header.js
@@ -2,7 +2,7 @@
 const Header = () => {
     return(
         <header>
-            <img src='/monster-icon-jkime.png' alt='monster-icon' />
+            <img src='/updated-icon-jzkime.png' alt='monster-icon' />
             <div>
                 <h1>Random Character Generator!</h1>
                 <p>A site for character generation. Whether it be in devising inspiration for drawing or a story persona- from a robotic elf to a happy gnome with light magic, Creation Station is there to help in your artistic planning. </p>
